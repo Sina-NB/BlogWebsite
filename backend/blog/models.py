de858b1f3445607c2dee74218ea633f79c3f22ff
Sina Nabigol <sina.nabigol77@gmail.com>
2023-11-02 12:18:39 +0330

    Create post and category models

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    '''
+    This class implements post model for blog app
+    '''
+    title = models.CharField(max_length= 250)
+    content = models.TextField()
+    image = models.ImageField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField('Category')
+    status = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title 
+    
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+class Category(models.Model):
+    '''
+    This class implements category model for blog app
+    '''
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
